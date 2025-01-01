d001a3c11bf0b95ef1b5cdc8e53916119e0802fb
hvoking <gus@ugeom.com>
2025-01-01 15:17:45 -0300

    isochrone working

--- src/components/maps/mask/index.tsx
+++ src/components/maps/mask/index.tsx
@@ -1,10 +1,13 @@
+// React imports
+import { useState, useEffect } from 'react';
+
 // App imports
 import { CustomMarker } from './marker';
 import { Boundary } from './boundary';
 import { Layers } from './layers';
 
 // Context imports
-
+import { useIsochroneApi } from 'context/api/isochrone';
 
 // Third party imports
 import * as turf from '@turf/turf';
@@ -13,12 +16,25 @@ export const Mask = ({ marker }: any) => {
   const { id, longitude, latitude } = marker;
   const center = [ longitude, latitude ];
   const boundary = turf.circle(center, 0.5);
-  
+
+  const { fetchIsochrone } = useIsochroneApi();
+  const [ isochroneData, setIsochroneData ] = useState<any>(null);
+
+  useEffect(() => {
+      const fetchData = async (marker: any) => {
+          const { longitude, latitude } = marker;
+          const data = await fetchIsochrone(longitude, latitude);
+          setIsochroneData(data);
+      };
+
+      fetchData(marker)
+  }, [ marker ]);
+
   return (
     <div key={id}>
-      <Boundary marker={marker} boundary={boundary}/>
+      {isochroneData && <Boundary marker={marker} data={isochroneData.features[0]}/>}
       <CustomMarker marker={marker}/>
-      <Layers marker={marker} boundary={boundary} />
+      {isochroneData && <Layers marker={marker} boundary={isochroneData.features[0]} />}
     </div>
   )
 };
