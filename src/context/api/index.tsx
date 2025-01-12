29cf5148c34d413259fe8d386f065da166460816
hvoking <gus@ugeom.com>
2025-01-12 06:45:50 -0300

    added bedrock

--- src/context/api/index.tsx
+++ src/context/api/index.tsx
@@ -1,13 +1,16 @@
 import { GoogleApiProvider } from './google';
 import { StylesApiProvider } from './styles';
 import { IsochroneApiProvider } from './isochrone';
+import { BedrockApiProvider } from './bedrock';
 
 export const ApiProvider = ({ children }: any) => {
 	return (
 		<GoogleApiProvider>
 		<StylesApiProvider>
 		<IsochroneApiProvider>
+		<BedrockApiProvider>
 			{children}
+		</BedrockApiProvider>
 		</IsochroneApiProvider>
 		</StylesApiProvider>
 		</GoogleApiProvider>
