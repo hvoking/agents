717159dde50acbc52a77f6dd176383b88ce5cb06
hvoking <gus@ugeom.com>
2025-05-07 19:07:39 -0300

    backup 07-05-25

--- src/context/api/rag/index.tsx
+++ src/context/api/rag/index.tsx
@@ -6,13 +6,13 @@ const RagApiContext: React.Context<any> = createContext(null)
 export const useRagApi = () => useContext(RagApiContext)
 
 export const RagApiProvider = ({children}: any) => {
-	const fetchRag = async (question: any, knowledgeBase: any) => {
+	const fetchRag = async (question: any, metaData: any) => {
 		const res = await fetch(`${process.env.REACT_APP_API_URL}/rag`, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
 				question,
-				knowledge_base: JSON.stringify(knowledgeBase),
+				meta_data: JSON.stringify(metaData),
 			}),
 		});
 		const receivedData = await res.json();
