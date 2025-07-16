c67d3afae61c5e87892e62e729dbc28eecbf844a
hvoking <gus@ugeom.com>
2025-07-16 00:15:55 -0300

    backup 16-07-25

--- src/app/panel/menu/section/index.tsx
+++ src/app/panel/menu/section/index.tsx
@@ -7,13 +7,17 @@ import { useMarkers } from 'context/markers';
 export const Section = ({ section, title }: any) => {
 	const { activePage, setActivePage } = useMarkers();
 	const isActiveSection = activePage === section ? "active" : "";
-	const iconPath = `${process.env.PUBLIC_URL}/static/icons/${section}.svg`;
+	const iconPath = `${process.env.PUBLIC_URL}/static/panel/${section}.svg`;
+	
+	const onClick = () => 
+		activePage !== section ? 
+		setActivePage(section) : 
+		setActivePage(null);
+
+	const itemClass = `menu-item ${isActiveSection}`;
 
 	return (
-		<div 
-			className={`menu-item ${isActiveSection}`} 
-			onClick={() => setActivePage(section)}
-		>
+		<div className={itemClass} onClick={onClick}>
 			<img src={iconPath} alt={title} width="30px"/>
 			<span className="section-text">{title}</span>
 		</div>
